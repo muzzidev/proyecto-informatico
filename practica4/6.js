0ef1b26b38ef2406457d23203e83be22f66d76fa
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-06-19 16:32:05 -0300

    modifica 6

--- practica4/6.js
+++ practica4/6.js
@@ -1,6 +1,10 @@
-let element = document.getElementById("#input");
-let contenido = parseInt(element.textContent);
-function isLeap(contenido) {
-    return (contenido % 4 == 0 && contenido % 100 != 0) || (contenido % 400 == 0) ? "Es bisiesto" : "No es bisiesto";
-}
-isLeap(2024);
+let boton = document.getElementById("boton");
+element.addEventListener(boton, () => {
+    function isLeap(contenido) {
+        document.append(numero);
+        let element = document.getElementById("input");
+        let contenido = parseInt(element.textContent);
+        let numero = document.createElement("h3");
+        return (contenido % 4 == 0 && contenido % 100 != 0) || (contenido % 400 == 0) ? "Es bisiesto" : "No es bisiesto";
+    }
+});
