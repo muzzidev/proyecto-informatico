057ef89b3fbeecabae634f7032761d1d90f92476
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-06-20 14:57:47 -0300

    agrego interfaz gráfica e imagen

--- practica4/6/6.js
+++ practica4/6/6.js
@@ -1,9 +1,18 @@
-let boton = document.getElementById("boton");
+let formulario = document.querySelector(".form");
+let botonLimpiar = document.querySelector(".boton-limpiar")
+
+formulario.addEventListener("submit", (event) => isLeap(event))
+botonLimpiar.addEventListener("click", (event) => clean(event));
 
 function isLeap(event) {
     event.preventDefault();
     let contenido = event.target.num.value;
-    let numero = document.createElement("h3");
-    numero.textContent = (contenido % 4 == 0 && contenido % 100 != 0) || (contenido % 400 == 0) ? "Es bisiesto" : "No es bisiesto";
-    event.target.append(numero);
+    let es_bisiesto = document.createElement("h3");
+    es_bisiesto.textContent = (contenido % 4 == 0 && contenido % 100 != 0) || (contenido % 400 == 0) ? "Es bisiesto" : "No es bisiesto";
+    event.target.append(es_bisiesto);
+}
+    
+function clean(event) {
+    event.preventDefault();
+    document.querySelector("h3").remove();
 }
