c552233b26008479498fa398ad55b51ab55edd2a
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-06-21 23:22:57 -0300

    termina el ejercicio 3

--- practica 4 - Javascript/1 - 9/3/3.js
+++ practica 4 - Javascript/1 - 9/3/3.js
@@ -3,21 +3,36 @@ cuatro bandas. Los argumentos son los dos primeros dígitos y el multiplicador. 
 el valor numérico en Ohms de la resistencia. Los colores en orden del 0 al 9 son: negro, marrón, rojo, naranja,
 amarillo, verde, azul, violeta, gris y blanco. No usar if ... else.*/
 
-const argv = process.argv.slice(2);
-function valorResistencia(b1, b2, b3){
-  let colores = {
-    negro: 0,
-    marron: 1,
-    rojo: 2,
-    naranja: 3,
-    amarillo: 4,
-    verde: 5,
-    azul: 6,
-    violeta: 7,
-    gris: 8,
-    blanco: 9    
-  }
-  return (colores[b1]*10 + colores[b2]) * 10 ** b3 + " Ohms";
+let formulario = document.querySelector(".form")
+formulario.addEventListener("submit", event => {
+  event.preventDefault();
+  let b1 = event.currentTarget.b1.value;
+  let b2 = event.currentTarget.b2.value;
+  let b3 = event.currentTarget.b3.value;
+  
+  if (document.querySelector(".resultado"))
+    document.querySelector(".resultado").remove();
+
+  let textoResultado = document.createElement("h1");
+  textoResultado.classList.add("resultado");
+  textoResultado.textContent = valorResistencia(b1, b2, b3);
+  document.body.append(textoResultado);
+})
+
+let colores = {
+  negro: 0,
+  marron: 1,
+  rojo: 2,
+  naranja: 3,
+  amarillo: 4,
+  verde: 5,
+  azul: 6,
+  violeta: 7,
+  gris: 8,
+  blanco: 9    
 }
 
-console.log(valorResistencia(argv[0], argv[1], argv[2]));
+function valorResistencia(b1, b2, b3){
+  let resultado = (colores[b1]*10 + colores[b2]) * (10 ** colores[b3]);
+  return (`${resultado} Ohms`);
+}
