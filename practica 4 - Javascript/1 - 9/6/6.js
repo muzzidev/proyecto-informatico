dd8d981b3e1edb22fda4081c58941db9a06b95e9
muzzidev <joaquin.hola.muzzi@gmail.com>
2024-07-06 22:13:15 -0300

    termina 6

--- practica 4 - Javascript/1 - 9/6/6.js
+++ practica 4 - Javascript/1 - 9/6/6.js
@@ -4,9 +4,9 @@ formulario.addEventListener("submit", (event) => isLeap(event))
 function isLeap(event) {
     event.preventDefault();
     if (document.querySelector(".resultado")) document.querySelector(".resultado").remove();
-    let contenido = event.target.num.value;
-    let es_bisiesto = document.createElement("h1");
-    es_bisiesto.classList.add("resultado");
-    es_bisiesto.textContent = (contenido % 4 == 0 && contenido % 100 != 0) || (contenido % 400 == 0) ? "Es bisiesto" : "No es bisiesto";
-    document.body.append(es_bisiesto);
+    let contenido = event.target.bien.checked;
+    let respuesta = document.createElement("h1");
+    respuesta.classList.add("resultado");
+    respuesta.textContent = (contenido ? "Eso es una verdad" : "Por qué me mentís");
+    document.body.append(respuesta);
 }
