67dda3eabf6e3d8fe3c7f20e38360a26518422dd
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-10-22 10:54:03 -0300

    agrega 29 sin terminar porque me tengo que ir a comprar

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -6,6 +6,7 @@ import Nav from './ejercicios/13 - 19/14.jsx';
 import H1 from './ejercicios/13 - 19/15.jsx';
 import Prod from './ejercicios/13 - 19/16.jsx';
 import Pokemon from './ejercicios/20 - 29/23.jsx';
+import Trivia from './ejercicios/20 - 29/29.jsx';
 import Todo from './ejercicios/Todo.jsx';
 
 import warcraft from './assets/warcraft.png'
@@ -23,6 +24,7 @@ function App() {
             <li><Link to="/ejercicio15">Ejercicio 15</Link></li>
             <li><Link to="/ejercicio16">Ejercicio 16</Link></li>
             <li><Link to="/ejercicio23">Ejercicio 23</Link></li>
+            <li><Link to="/ejercicio29">Ejercicio 29</Link></li>
             <li><Link to="/Todo">Todo app</Link></li>
           </ul>
         </nav>
@@ -41,6 +43,7 @@ function App() {
           />
           <Route path='/ejercicio23' element={<Pokemon />}/>
           <Route path='/Todo' element={<Todo />}/>
+          <Route path='/ejercicio29' element={<Trivia />}/>
         </Routes>
         
       </div>
