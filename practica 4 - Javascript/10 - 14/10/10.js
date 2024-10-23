dfacf887ab9005bbf1d04e556376433d3fb93805
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-10-23 01:07:13 -0300

    agrega 24 y uno de chat

--- practica 4 - Javascript/10 - 14/10/10.js
+++
@@ -1,8 +0,0 @@
-/*3. Crear un carousel usando HTML, CSS y JavaScript*/
-
-let elemento = document.querySelector(".menu")
-function openMenu() {
-  let nuevoElemento1 = document.createElement("div");
-  nuevoElemento1.classList.add("elemento");
-  elemento.append(nuevoElemento1);
-}
