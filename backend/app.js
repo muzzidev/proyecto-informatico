4c1545358aec638243625e0f1a10572607396002
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-11-11 23:59:44 -0300

    agrega cosas de backend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,102 @@
-import express from 'express'
-const app = express()
-const port = 3000
+/* 7. Escribir una API con dos colecciones: usuarios y comentarios. La API debe tener rutas para crear, consultar,
+modificar y borrar ambos recursos. Separar el código en rutas, controladores y modelos, cada uno en su archivo
+separado. */
 
-app.get('/', (req, res) => {
-  res.send("Hola Mundo");
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import morgan from 'morgan'
+
+
+const PORT = process.env.PORT = 3000;
+const DB   = process.env.DB = 'mongodb://127.0.0.1/api';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+//SE CONECTA
+mongoose.connect(DB)
+  .then(() => console.log('DB connected'));
+
+//ESQUEMAS DE LOS USUARIOS Y COMENTARIOS
+const UserSchema = new mongoose.Schema({
+  id: { type: Number, unique: true },
+  name: String
+})
+const CommentSchema = new mongoose.Schema({
+  id: { type: Number, unique: true },
+  title: String,
+  content: String
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+//EXPORTO LOS MODELOS DE LOS ESQUEMAS
+const User = mongoose.model('User', UserSchema);
+const Comment = mongoose.model('Comment', CommentSchema);
+
+//METODOS GET
+app.get('/api/comments', (req, res) => {
+  Comment.find()
+    .then(comments => res.status(200).json(comments)); 
+})
+
+app.get('/api/users', (req, res) => {
+  User.find()
+    .then(users => res.status(200).json(users)); 
+})
+
+app.get('/api/users/:id', (req, res) => {
+  User.findOne({ id: req.params.id })
+    .then(user => res.status(200).json(user)); 
+})
+
+app.get('/api/comments/:id', (req, res) => {
+  Comment.findOne({ id: req.params.id })
+    .then(comment => res.status(200).json(comment)); 
+})
+
+//METODOS POST
+
+app.post('/api/users', (req, res) => {
+  const newUser = new User(req.body);
+  newUser.save()
+    .then(() => res.status(200).json({ msg: 'User created' }));
+})
+
+app.post('/api/comments', (req, res) => {
+  const newComment = new Comment(req.body);
+  newComment.save()
+    .then(() => res.status(200).json({ msg: 'Comment created' }));
+})
+
+
+//METODOS DELETE
+
+app.delete('/api/users/:id', (req, res) => {
+  User.deleteOne({ id: req.params.id })
+    .then(() => res.status(200).json({ msg: 'User deleted!' }));
+});
+
+app.delete('api/comments/:id', (req, res) => {
+  Comment.deleteOne({ id: req.params.id })
+    .then(() => res.status(200).json({ msg: "Comment deleted" }));
+})
+
+//METODOS PUT
+app.put('api/comments/:id', (req, res) => {
+  Comment.findOneAndUpdate({ id: req.params.id }, req.body, { new: true })
+    .then(comment => res.status(200).json(comment));
+})
+
+
+//MIDDLEWAR DE ERRORES Y DE CONECCION A LA DB
+app.use((req, res) => {
+  res.status(404).json({ msg: 'Not found'});
+})
+
+app.listen(PORT, () => {
+  console.log('Server working');
+  
 })
---
+++ backend/machete.js
@@ -0,0 +1,75 @@
+// imports
+// const express = require('express'); CommonJS
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import cors from 'cors';
+// config vars
+const PORT = process.env.PORT || 3000;
+const DB   = process.env.DB   || 'mongodb://127.0.0.1/rickmorty';
+// objeto app
+const app = express();
+// middleware de aplicacion
+app.use(morgan('dev'));  // middleware de logs
+app.use(cors());
+app.use(express.json()); // parsea los bodys en JSON
+// conectar a la DB
+mongoose.connect(DB)
+  .then(() => console.log('DB conectada'));
+// esquemas y modelos
+const CharacterSchema = new mongoose.Schema({
+  id: { type: Number, unique: true },
+  name: String,
+  img: String,
+  species: String
+});
+const Character = mongoose.model('Character', CharacterSchema);
+// rutas de la API
+// todos los personajes
+// GET /api/characters
+app.get('/api/characters', (req, res) => {
+  Character.find()
+    .then(characters => res.status(200).json(characters)); // responde 200 OK
+});
+// GET /api/characters/search/:name
+app.get('/api/characters/search/:name', (req, res) => {
+  Character.find({ name: { "$regex": req.params.name, "$options": "i" } })
+    .then(characters => res.status(200).json(characters));
+    // .catch(err => res.status(403).json({ err: err.message }));
+});
+
+// un personaje por ID
+app.get('/api/characters/:id', (req, res) => {
+  Character.findOne({ id: req.params.id })
+    .then(character => res.status(200).json(character));
+});
+// app.get('/api/characters/id/:id', (req, res) => {
+//   Character.find({ id: req.params.id })
+//     .then(characters => res.status(200).json(characters));
+// });
+// crear un personaje 
+app.post('/api/characters', (req, res) => {
+  console.log('El body vale: ', req.body);
+  const { id, name, img, species } = req.body;
+  const newCharacter = new Character({ id, name, img, species });
+  newCharacter.save()
+    .then(character => res.status(201).json(character)); // responde 201 Created
+});
+// borrar un personaje 
+app.delete('/api/characters/:id', (req, res) => {
+  Character.deleteOne({ id: req.params.id })
+    .then(() => res.status(200).json({ msg: 'Character deleted!' }));
+});
+// modificar un personaje
+app.put('/api/characters/:id', (req, res) => {
+  Character.findOneAndUpdate({ id: req.params.id }, req.body, { new: true })
+    .then(character => res.status(200).json(character));
+}); 
+// error 404
+app.use((req, res) => {
+  res.status(404).json({ msg: 'No encontrado' }); // responde 404 Not found
+});
+// listen
+app.listen(PORT, () => {
+  console.log('Server andando');
+});
